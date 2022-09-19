c064432cbbbde6fb4cfaf0c40c487bcedc356ffa
tejasdobariya <dobariyatejas15@gmail.com>
2022-09-19 23:13:18 +0530

    labs

---
+++ login_signup/lib/Screens/LoginForm.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'HomeForm.dart';
+import 'SignupForm.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.grey[200],
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text("Login",
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 20,),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      inputFile(label: "EMAIL"),
+                      inputFile(label: "PASSWORD", obscureText: true)
+                    ],
+                  ),
+                ),
+                Padding(padding:
+                EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 3, left: 3),
+                    decoration:
+                    BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                        )
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> Home()));
+                      },
+                      color: Color(0xff0095FF),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Text(
+                        "Login", style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?"),
+                    GestureDetector(
+                      child: Text("SignUp", style:TextStyle(fontWeight: FontWeight.w600, fontSize: 18,),),
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+                      },
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 100),
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/Key-Lock.jpg"),
+                        fit: BoxFit.fitHeight
+                    ),
+                  ),
+                )
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget inputFile({label, obscureText = false})
+{
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        label,
+        style: TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color:Colors.black87
+        ),
+      ),
+      SizedBox(
+        height: 5,
+      ),
+      TextField(
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0,
+                horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: Colors.grey
+              ),
+            ),
+            border: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey)
+            )
+        ),
+      ),
+      SizedBox(height: 10,)
+    ],
+  );
+}
---
+++ login_signup/lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'Screens/LoginForm.dart';
+import 'Screens/SignupForm.dart';
+import 'dart:ui';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric( vertical: 150,horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/Lock.jpg")
+                    )
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
+
+                    },
+                    color: Color(0xff0095FF),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  SizedBox(height:20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
