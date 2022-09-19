bcffa27f89c8a1903cf2f6cca58593afa3ce9af9
tejasdobariya <dobariyatejas15@gmail.com>
2022-09-19 22:31:55 +0530

    labs

---
+++ login/lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:login/screens/LoginForm.dart';
+import 'dart:ui';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(vertical: 100,horizontal: 40),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+
+                    ),
+
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/Lock.jpg")
+                    )
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+
+                          fontWeight: FontWeight.w600,
+                          fontSize: 23
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                ],
+              )
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ login/lib/screens/Home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent[600],
+      ),
+
+      body: Center(
+        child: Text(
+          'You have login Successfully',
+          style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'Aboreto'
+          ),
+        ),
+      ),
+    );
+  }
+}
