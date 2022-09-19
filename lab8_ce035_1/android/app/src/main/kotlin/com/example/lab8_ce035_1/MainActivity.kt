196613acd0c4f5b6b68c4275dc7f4a43462f6a05
tejasdobariya <dobariyatejas15@gmail.com>
2022-09-19 21:15:01 +0530

    labs

---
+++ lab8_ce035_1/android/app/src/main/kotlin/com/example/lab8_ce035_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab8_ce035_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lab8_ce035_1/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() => runApp(MaterialApp(
+  home: EchoList(),
+));
+class EchoList extends StatefulWidget {
+  const EchoList({Key? key}) : super(key: key);
+  @override
+  State<EchoList> createState() => _EchoListState();
+}
+class _EchoListState extends State<EchoList> {
+  List<Quote> quotes = [
+    Quote(text: 'The truth is realy pure and never simple',author:
+    'Tejas'),
+
+    Quote(author: 'Kashyap', text: 'I see humans but no humanity'),
+    Quote(text: 'The time is always right to do what is right',author:
+    'Smit'),
+  ];
+  Widget quoteTemplate(quote){
+    return Card(
+      margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 20,
+
+                color: Colors.deepPurple,
+
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 26,
+
+                color: Colors.deepPurple,
+
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent[100],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+      ),
+    );
+  }
+}
